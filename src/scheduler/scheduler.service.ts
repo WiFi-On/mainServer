435973b2fa55d6c83d2fba7db6ee6b3a219b1345
vlad <vladik00848@gmail.com>
2025-02-03 17:11:48 +0500

    Исправил баги и немного логирование

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -7,7 +7,7 @@ import { BitrixService } from 'src/bitrix/bitrix.service';
 @Injectable()
 export class SchedulerService {
   private readonly enviroment: string;
-  private readonly logger = new Logger(EissdService.name);
+  private readonly logger = new Logger(SchedulerService.name);
   constructor(
     @Inject(ConfigService) private readonly configService: ConfigService,
     private readonly eissdService: EissdService,
@@ -19,7 +19,7 @@ export class SchedulerService {
   async onModuleInit(): Promise<void> {
     if (this.enviroment === 'prod') {
       await this.creatingApplications();
-      // await this.editStatusApplication();
+      await this.editStatusApplication();
     }
   }
 
@@ -35,8 +35,8 @@ export class SchedulerService {
     if (this.enviroment === 'prod') {
       // Получаем заявки по ростелекому из битрикса
       const leadsBitrixRtk = await this.bitrixService.getDeals(BitrixStatuses.toSent, 52);
-      this.logger.log(`Получены заявки по ростелекому из битрикса. Количество заявок: ${leadsBitrixRtk.length}`, {
-        path: 'scheduler/creatingApplications',
+      this.logger.log(`Получены заявки по ростелекому из битрикса для автозаведения. Количество заявок: ${leadsBitrixRtk.length}`, {
+        context: 'scheduler/creatingApplications',
         resultCount: leadsBitrixRtk.length,
       });
       for (const lead of leadsBitrixRtk) {
@@ -50,7 +50,6 @@ export class SchedulerService {
             // Если при создании заявки что то пошло не так, то возвращаем ошибку в битрикс в комментарии.
             if (application.err) {
               this.logger.error(`Заявка не заведена. Адрес: ${lead.address}. Причина: ${application.result}`, {
-                context: 'EissdService',
                 address: lead.address,
                 path: 'scheduler/creatingApplications',
                 result: application.result,
@@ -67,7 +66,6 @@ export class SchedulerService {
           // если нет технических возможности, то отправляем в битрикс с комментрарием проверить техническую возможность
           else {
             this.logger.error(`Нужно проверить тхв вручную. Адрес: ${lead.address}`, {
-              context: 'EissdService',
               address: lead.address,
               path: 'scheduler/creatingApplications',
               result: thv.result.thv,
@@ -76,7 +74,6 @@ export class SchedulerService {
           }
         } catch (error) {
           this.logger.error(`Ошибка при создании заявки. Адрес: ${lead.address}. Причина: ${error.message}`, {
-            context: 'EissdService',
             address: lead.address,
             path: 'scheduler/creatingApplications',
             result: error.message,
@@ -91,65 +88,65 @@ export class SchedulerService {
    * Проверяет статус заявки через EISSD и в зависимости от статуса заявки меняет статус в битриксе.
    * @returns {Promise<void>}
    */
-  // @Cron('0 */3 * * *')
+  @Cron('0 */4 * * *')
   async editStatusApplication(): Promise<void> {
     if (this.enviroment === 'prod') {
-      const leadsBitrix = await this.bitrixService.getDeals(BitrixStatuses.toAppointed);
-      for (const lead of leadsBitrix) {
+      const leadsBitrixRtk = await this.bitrixService.getDeals(BitrixStatuses.toAppointed, 52);
+      this.logger.log(`Получены заявки по ростелекому из битрикса для изменения статуса. Количество заявок: ${leadsBitrixRtk.length}`, {
+        resultCount: leadsBitrixRtk.length,
+      });
+      for (const lead of leadsBitrixRtk) {
         try {
-          const statusApplication = await this.eissdService.getStatusesApplication(lead.id);
+          const statusApplication = await this.eissdService.getStatusesApplication(lead.application_id);
           const serviceInternet = statusApplication.find((service) => service.serviceId === '1');
           if (!serviceInternet.statusReasonId && serviceInternet.statusId === '37') {
             this.logger.log(`Назначены дата и время инсталляции. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-              context: 'scheduler/editStatusApplication',
               idBitrix: lead.id,
               idApplication: lead.application_id,
               serviceInternet: serviceInternet,
             });
             continue;
           } else if (!serviceInternet.statusReasonId && serviceInternet.statusId === '7') {
-            this.bitrixService.editApplication(lead.id, undefined, lead.application_id, BitrixStatuses.toConnected);
+            const commentBitrix = 'Статус: ' + serviceInternet.statusName + '\n' + 'Дополнительный статус: ' + serviceInternet.statusReasonName;
+            this.bitrixService.editApplication(lead.id, commentBitrix, lead.application_id, BitrixStatuses.toConnected);
             this.logger.log(`Клиент подключен. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-              context: 'scheduler/editStatusApplication',
               idBitrix: lead.id,
               idApplication: lead.application_id,
               serviceInternet: serviceInternet,
             });
           } else if (serviceInternet.bitrixStatus == 'Заявка назначена') {
             this.logger.log(`Заявка назначена. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-              context: 'scheduler/editStatusApplication',
               idBitrix: lead.id,
               idApplication: lead.application_id,
               serviceInternet: serviceInternet,
             });
             continue;
           } else if (serviceInternet.bitrixStatus == 'Отработка заявок') {
-            this.bitrixService.editApplication(lead.id, undefined, lead.application_id, BitrixStatuses.toWorkingOff);
+            const commentBitrix = 'Статус: ' + serviceInternet.statusName + '\n' + 'Дополнительный статус: ' + serviceInternet.statusReasonName;
+            this.bitrixService.editApplication(lead.id, commentBitrix, lead.application_id, BitrixStatuses.toWorkingOff);
             this.logger.log(`На отработку. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-              context: 'scheduler/editStatusApplication',
               idBitrix: lead.id,
               idApplication: lead.application_id,
               serviceInternet: serviceInternet,
             });
           } else if (serviceInternet.bitrixStatus == 'Отказ') {
-            this.bitrixService.editApplication(lead.id, serviceInternet.bitrixCause, lead.application_id, BitrixStatuses.toRefusal);
+            const commentBitrix = 'Статус: ' + serviceInternet.statusName + '\n' + 'Дополнительный статус: ' + serviceInternet.statusReasonName;
+            this.bitrixService.editApplication(lead.id, commentBitrix, lead.application_id, BitrixStatuses.toRefusal);
             this.logger.log(`Отказ. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-              context: 'scheduler/editStatusApplication',
               idBitrix: lead.id,
               idApplication: lead.application_id,
               serviceInternet: serviceInternet,
             });
           } else if (serviceInternet.bitrixStatus == 'Клиент подключен') {
-            this.bitrixService.editApplication(lead.id, undefined, lead.application_id, BitrixStatuses.toConnected);
+            const commentBitrix = 'Статус: ' + serviceInternet.statusName + '\n' + 'Дополнительный статус: ' + serviceInternet.statusReasonName;
+            this.bitrixService.editApplication(lead.id, commentBitrix, lead.application_id, BitrixStatuses.toConnected);
             this.logger.log(`Клиент подключен. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-              context: 'scheduler/editStatusApplication',
               idBitrix: lead.id,
               idApplication: lead.application_id,
               serviceInternet: serviceInternet,
             });
           } else {
             this.logger.error(`Ошибка в изменении статуса заявки в битрикс. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-              context: 'scheduler/editStatusApplication',
               idBitrix: lead.id,
               idApplication: lead.application_id,
               serviceInternet: serviceInternet,
@@ -157,7 +154,6 @@ export class SchedulerService {
           }
         } catch (error) {
           this.logger.error(`Ошибка в изменении статуса заявки в битрикс. Айди битрикс: ${lead.id}. Айди заявки: ${lead.application_id}.`, {
-            context: 'scheduler/editStatusApplication',
             idBitrix: lead.id,
             idApplication: lead.application_id,
             error: error,
